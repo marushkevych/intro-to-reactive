475fd2cfde25efe3d3668e61e8cd7770c060e8f2
Andrey Marushkevych <marushkevych@gmail.com>
2017-03-09 22:33:41 -0500

    works wiht local storage wrapper

---
+++ index.js
@@ -0,0 +1,86 @@
+const choo = require('choo')
+const html = require('choo/html')
+const extend = require('./extend')
+const store = require('./localStorage')
+
+const app = choo()
+
+app.model({
+  state: {
+    todos: []
+  },
+  reducers: {
+    receiveNewTodo: (state, data) => {
+      return {todos: [...state.todos, data]}
+    },
+    replaceTodo: (state, {index, todo}) => {
+      const todos = state.todos.slice()
+      todos[index] = todo
+      return {todos}
+    },
+    receiveTodos: (state, data) => ({todos: data})
+  },
+  effects: {
+    getTodos: (state, data, send, done) => {
+      store.getAll('todos', (todos) => {
+        send('receiveTodos', todos, done)
+      })
+    },
+    addTodo: (state, data, send, done) => {
+      store.add('todos', data, () => {
+        send('receiveNewTodo', data, done)
+      })
+    },
+    toggle: (state, index, send, done) => {
+      const todos = state.todos.slice()
+      const oldTodo = todos[index]
+      const todo = extend(oldTodo, {done: !oldTodo.done})
+      store.replace('todos', index, todo, () => {
+        send('replaceTodo', {index, todo}, done)
+      })
+    },
+  }
+})
+
+const view = (state, prev, send) => {
+  return html`
+    <div onload=${() => send('getTodos')}>
+      <h1>Todo</h1>
+      <form onsubmit=${onSubmit}>
+        <input type="text" placeholder="New Item" id="title">
+      </form>
+      <ul>
+        ${state.todos.map(renderTodo)}
+      </ul>
+    </div>`
+
+  function renderTodo(todo, index) {
+    const todoEntry = html`
+      <label>
+        <input type="checkbox" onchange=${onChange} ${todo.done ? 'checked' : ''}/>
+        ${todo.title}
+      </label>`
+
+    const todoEntryDecorated = todo.done ? html`<strike>${todoEntry}</strike>` : todoEntry
+
+    return html`<li>${todoEntryDecorated}</li>`
+
+    function onChange() {
+      send('toggle', index)
+    }
+  }
+
+  function onSubmit(e) {
+    const input = e.target.children[0]
+    send('addTodo', {title: input.value})
+    input.value = ''
+    e.preventDefault()
+  }
+}
+
+app.router([
+  ['/', view]
+])
+
+const tree = app.start()
+document.body.appendChild(tree)
---
+++ localStorage.js
@@ -0,0 +1,26 @@
+// localStorage wrapper
+const store = {
+  getAll: (storeName, cb) => {
+    try {
+      cb(JSON.parse(window.localStorage[storeName]))
+    } catch (e) {
+      cb([])
+    }
+  },
+  add: (storeName, item, cb) => {
+    store.getAll(storeName, (items) => {
+      items.push(item)
+      window.localStorage[storeName] = JSON.stringify(items)
+      cb()
+    })
+  },
+  replace: (storeName, index, item, cb) => {
+    store.getAll(storeName, (items) => {
+      items[index] = item
+      window.localStorage[storeName] = JSON.stringify(items)
+      cb()
+    })
+  }
+}
+
+module.exports = store
